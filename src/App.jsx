4cc281b4141b7f881f5377be0b00a14ad125eb22
David An <david.an@virbela.com>
2022-04-23 23:56:12 -0400

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import './App.css';
 import Home from "./pages/Home";
@@ -9,18 +9,17 @@ import Signout from "./pages/Signout";
 import Layout from "./pages/Layout";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path='home' element={<Home />} />
-          <Route path="callback" element={<Callback />} />
-          <Route path="signout" element={<Signout />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return (<>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/callback" element={<Callback />} />
+        <Route path="/signout" element={<Signout />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </>);
 }
 
 export default App;
