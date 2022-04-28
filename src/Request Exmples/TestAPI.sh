62c857a85a185973b039781b10a0cf2d4c5fc6a3
David An <david.an@virbela.com>
2022-04-27 20:47:06 -0400

    Remove unused files

--- src/Request Exmples/TestAPI.sh
+++
@@ -1,4 +0,0 @@
-curl --location --request \
-GET \
-'https://x11x9w39f8.execute-api.us-east-2.amazonaws.com/Dev/store?apiname=worldspace' \
---header 'Authorization: Bearer {****your_access_token****}' \
